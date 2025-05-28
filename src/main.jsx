dc4e198f7539525d2579ba28c3aa425a39c0b40b
Georgios Koutoulas <71599369+georgekoutoulas@users.noreply.github.com>
2025-05-28 16:17:50 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import WeddingInvitation from './WeddingInvitation.jsx';
+import AnimatedSequence from './AnimatedSequence.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

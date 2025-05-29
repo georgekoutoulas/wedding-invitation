ac743cf0bd8dbb978bc910ba75761a21a9006375
Georgios Koutoulas <71599369+georgekoutoulas@users.noreply.github.com>
2025-05-28 22:42:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 👈 Προσθέτει σωστά το path για στατικά αρχεία
   plugins: [react()],
 });
